53276eef2d159437717074134e99a3c309f5666e
damien75 <damiengoblot@gmail.com>
2015-11-19 20:27:35 +0000

    Background test: Adding movies to the DB

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,11 +1,10 @@
 # Add a declarative step here for populating the DB with movies.
-
-Given /the following movies exist/ do |movies_table|
+Given /^the following movies exist:$/ do |movies_table|
   movies_table.hashes.each do |movie|
+    Movie.create!(movie)
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +13,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  p1 = page.body.find(e1)
+  p2 = page.body.find(e2)
+  assert(p1 < p2)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
